1453aa77af45a4cdedce3132f0c6434658fe5356
Al-Inayya <113782244+Al-Inayya@users.noreply.github.com>
2022-11-01 19:48:56 +0800

    Add files via upload

---
+++ Posttest6/config.php
@@ -0,0 +1,12 @@
+<?php
+ 
+ //jangan lupa import sqlnya dulu :)
+
+$databaseHost = 'localhost';
+$databaseName = '127naya';
+$databaseUsername = 'root';
+$databasePassword = '';
+ 
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+ 
+?>
